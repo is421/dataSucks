d7b1d01d1e429584f38e17d1d2e4c9526258e9b9
ortegaj716 <ortegaj716@gmail.com>
2014-11-13 19:40:41 +0000

    Added cookie parser + OAUTH secret. Fixed layout error and consumer string

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var express = require('express')
   , TwitterStrategy = require('passport-twitter').Strategy;
 
 passport.use(new TwitterStrategy({
-    consumerKey: EUGbXnHc7TSlFZxkHp69i0l7y,
-    consumerSecret: fSmqoroZtrybNHYSehI0U3iEWoPzHNLSz6Nxb4EyLoHAxxiGIZ,
+    consumerKey: "EUGbXnHc7TSlFZxkHp69i0l7y",
+    consumerSecret: "fSmqoroZtrybNHYSehI0U3iEWoPzHNLSz6Nxb4EyLoHAxxiGIZ",
     callbackURL: "http://puppet.srihari.guru:3000/auth/twitter/callback"
   },
   function(token, tokenSecret, profile, done) {
@@ -27,6 +27,8 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(express.cookieParser());
+  app.use(express.session({secret: 'ABSOLUTELYWITHOUTADOUBTTHESAFESTSECRETEVERMADE'}));
 });
 
 app.configure('development', function(){
@@ -50,6 +52,10 @@ app.get('/auth/twitter/callback', passport.authenticate('twitter', {
   successRedirect: '/auth/twitter/youdidit',
   failureRedirect: '/'}));
 
+//Test!
+app.get('/auth/twitter/youdidit', function(req,res){
+  res.send('You did it!');
+});
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
