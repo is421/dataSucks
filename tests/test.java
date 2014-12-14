9fa47fa4401e6eb462908f2f6d5bd3c9d09de6dc
Keroles Anwar <keroles.anwar@gmail.com>
2014-12-13 20:25:30 -0500

    Changed Structure; static mainLogin method since it is necessary for the other tests

--- tests/test.java
+++ tests/test.java
@@ -6,33 +6,44 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  
 public class test {
 
+     static String baseUrl="http://localhost";
+
+     public static WebDriver mainLogin(){
+	
+	//User inputs Account info
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter a valid username (email)");
+	String name = scan.nextLine();
+	System.out.println("Enter a valid password");
+	String pass = scan.nextLine();
+
+	WebDriver driver = new FirefoxDriver();
+
+
+	//Open firefox on localhost
+		driver.get(baseUrl);
+
+	//Locate "username", "password" text boxes and submit button
+
+	WebElement username = driver.findElement(By.name("username"));
+	WebElement password = driver.findElement(By.name("password"));
+	WebElement btn = driver.findElement(By.cssSelector("input[value=\"Signin\"]"));
+
+	//Enter and submit user info
+	username.sendKeys(name);
+	password.sendKeys(pass);
+	btn.submit();
+	return driver;
+
+    }
+
     public static void main(String[] args) {
         
-        //User inputs Account info
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a valid username (email)");
-        String name = scan.nextLine();
-        System.out.println("Enter a valid password");
-        String pass = scan.nextLine();
-        
-    	//Open firefox on localhost
-        WebDriver driver = new FirefoxDriver();
-        String baseUrl="http://localhost:80";
-        driver.get(baseUrl);
-        
-        //Locate "username", "password" text boxes and submit button
-
-        WebElement username = driver.findElement(By.name("username"));
-        WebElement password = driver.findElement(By.name("password"));
-        WebElement btn = driver.findElement(By.cssSelector("input[value=\"Signin\"]"));
-        
-        //Enter and submit user info
-        username.sendKeys(name);
-        password.sendKeys(pass);
-        btn.submit();
-        
+        WebDriver driver = mainLogin();
         //Check if successful 
-    	}
-
- 
+        if(driver.getCurrentUrl().equals(baseUrl + "/dashboard")){
+        	System.out.println("Success!");
+        }
+        else System.out.println("Fail");
+    }
 }
